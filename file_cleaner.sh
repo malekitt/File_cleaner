8c9de34a7c731af9958015c1188575500eee62ae
malekitt <vojta.kollmann@gmail.com>
2025-12-04 10:32:27 +0100

    Create file_cleaner.sh

---
+++ file_cleaner.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#--------------------------------------------------------------------------
+# File_cleaner pomáhá třídit dokumenty, které stáhnete do stažených souborů
+#    - uložíte soubor do stažených souborů
+#    - skript přesune soubor do příslušné složky podle koncovky souboru
+#    - do složky vloží max 5 stejných souborů
+#    - plnou složku označí barevně a vytvoří novou složku slozka2
+#    - barvy složek jsou podle typu souboru
+#---------------------------------------------------------------------------
+
